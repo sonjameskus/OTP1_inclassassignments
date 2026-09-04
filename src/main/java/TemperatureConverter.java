821f6c3dbf32a5323e40d60d81fbc426e4ce260e
sonjameskus <sonja.meskus@metropolia.fi>
2026-09-04 21:11:38 +0300

    in class assignment 1

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class TemperatureConverter {
+
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public boolean isExtremeTemperature(double celsius) {
+        return celsius < -40 || celsius > 50;
+    }
 }
--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -1,4 +1,34 @@
 import static org.junit.jupiter.api.Assertions.*;
-class TemperatureConverterTest {
-  
+import org.junit.jupiter.api.Test;
+
+public class TemperatureConverterTest {
+
+    @Test
+    void testFahrenheitToCelsius() {
+        TemperatureConverter converter = new TemperatureConverter();
+
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100, converter.fahrenheitToCelsius(212), 0.001);
+        assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.001);
+    }
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        TemperatureConverter converter = new TemperatureConverter();
+
+        assertEquals(32, converter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212, converter.celsiusToFahrenheit(100), 0.001);
+        assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.001);
+    }
+
+    @Test
+    void testIsExtremeTemperature() {
+        TemperatureConverter converter = new TemperatureConverter();
+
+        assertFalse(converter.isExtremeTemperature(-40));
+        assertTrue(converter.isExtremeTemperature(-40.1));
+
+        assertFalse(converter.isExtremeTemperature(50));
+        assertTrue(converter.isExtremeTemperature(50.1));
+    }
 }
